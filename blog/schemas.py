c5fd4dd36b38fad6d83e53b512294ac338461d9f
Lalasa <lalasagidugu55@gmail.com>
2026-03-18 16:25:12 +0530

    Final schema fix

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,24 +1,11 @@
 from pydantic import BaseModel, ConfigDict
 from typing import List, Optional
 
-
 class BlogBase(BaseModel):
     title: str
     body: str
 
 class Blog(BlogBase):
-    model_config = ConfigDict(from_attributes=True)
-
-class ShowUserForBlog(BaseModel):
-    name: str
-    email: str
-    model_config = ConfigDict(from_attributes=True)
-
-
-class ShowBlog(BaseModel):
-    title: str
-    body: str
-    creator: ShowUserForBlog
     model_config = ConfigDict(from_attributes=True)
 
 class User(BaseModel):
@@ -27,11 +14,28 @@ class User(BaseModel):
     password: str
     model_config = ConfigDict(from_attributes=True)
 
+class ShowUserForBlog(BaseModel):
+    name: str
+    email: str
+    model_config = ConfigDict(from_attributes=True)
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUserForBlog
+    model_config = ConfigDict(from_attributes=True)
+
 class ShowUser(BaseModel):
     name: str
     email: str
     blogs: List[Blog] = [] 
     model_config = ConfigDict(from_attributes=True)
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
 
 ShowUser.model_rebuild()
