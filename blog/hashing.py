252eae128cbb084ecab6f5bdd54fc11431086c65
Lalasa <lalasagidugu55@gmail.com>
2026-03-21 17:30:15 +0530

    fix bcrypt error

--- blog/hashing.py
+++ blog/hashing.py
@@ -5,8 +5,8 @@ pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash():
     @staticmethod
     def bcrypt(password: str):
-        return pwd_cxt.hash(password)
+        return pwd_cxt.hash(password[:72])  
 
     @staticmethod
     def verify(hashed_password: str, plain_password: str):
-        return pwd_cxt.verify(plain_password, hashed_password)
+        return pwd_cxt.verify(plain_password[:72], hashed_password)
